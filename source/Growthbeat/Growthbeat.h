ea9f940e8ca827755ac46468b427762fa0dc5aab
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-08-10 00:58:01 +0900

    Add library project

---
+++ source/Growthbeat/Growthbeat.h
@@ -0,0 +1,13 @@
+//
+//  Growthbeat.h
+//  Growthbeat
+//
+//  Created by Kataoka Naoyuki on 2014/08/10.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Growthbeat : NSObject
+
+@end
---
+++ source/Growthbeat/Growthbeat.m
@@ -0,0 +1,13 @@
+//
+//  Growthbeat.m
+//  Growthbeat
+//
+//  Created by Kataoka Naoyuki on 2014/08/10.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import "Growthbeat.h"
+
+@implementation Growthbeat
+
+@end
