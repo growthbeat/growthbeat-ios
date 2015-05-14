825c6ef6d1b89f4763fe514bb0d4c126fb7bfae4
Shigeru <ogawa@sirok.co.jp>
2015-05-14 16:02:10 +0900

    Change version 1.0.0

---
+++ Growthbeat.framework/Versions/1.0.0/Headers/Growthbeat.h
@@ -0,0 +1,26 @@
+//
+//  Growthbeat.h
+//  Growthbeat
+//
+//  Created by Kataoka Naoyuki on 2014/08/10.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GrowthbeatCore.h"
+#import "GrowthAnalytics.h"
+#import "GrowthMessage.h"
+
+@interface Growthbeat : NSObject
+
++ (instancetype)sharedInstance;
+
+- (void)initializeWithApplicationId:(NSString *)initialApplicationId credentialId:(NSString *)initialCredentialId;
+- (void)initializeGrowthAnalytics;
+- (void)initializeGrowthMessage;
+
+- (void)start;
+- (void)stop;
+- (void)setLoggerSilent:(BOOL)silent;
+
+@end
