5c96a08b355923be06d1e2b45cda02f53ebfc4c1
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-05-29 12:31:56 +0900

    Add Growth Link SDK

---
+++ source/GrowthLink/GrowthLink/GrowthLink.m
@@ -0,0 +1,95 @@
+//
+//  GrowthLink.m
+//  GrowthLink
+//
+//  Created by Naoyuki Kataoka on 2015/05/29.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import "GrowthLink.h"
+#import "GrowthAnalytics.h"
+
+static GrowthLink *sharedInstance = nil;
+static NSString *const kGBLoggerDefaultTag = @"GrowthLink";
+static NSString *const kGBHttpClientDefaultBaseUrl = @"https://api.link.growthbeat.com/";
+static NSTimeInterval const kGBHttpClientDefaultTimeout = 60;
+static NSString *const kGBPreferenceDefaultFileName = @"growthlink-preferences";
+
+@interface GrowthLink () {
+
+    GBLogger *logger;
+    GBHttpClient *httpClient;
+    GBPreference *preference;
+
+    NSString *applicationId;
+    NSString *credentialId;
+
+    BOOL initialized;
+
+}
+
+@property (nonatomic, strong) GBLogger *logger;
+@property (nonatomic, strong) GBHttpClient *httpClient;
+@property (nonatomic, strong) GBPreference *preference;
+
+@property (nonatomic, strong) NSString *applicationId;
+@property (nonatomic, strong) NSString *credentialId;
+
+@property (nonatomic, assign) BOOL initialized;
+
+@end
+
+@implementation GrowthLink
+
+@synthesize logger;
+@synthesize httpClient;
+@synthesize preference;
+
+@synthesize applicationId;
+@synthesize credentialId;
+
+@synthesize initialized;
+
++ (instancetype) sharedInstance {
+    @synchronized(self) {
+        if ([[[UIDevice currentDevice] systemVersion] floatValue] < 5.0f) {
+            return nil;
+        }
+        if (!sharedInstance) {
+            sharedInstance = [[self alloc] init];
+        }
+        return sharedInstance;
+    }
+}
+
+- (id) init {
+    self = [super init];
+    if (self) {
+        self.logger = [[GBLogger alloc] initWithTag:kGBLoggerDefaultTag];
+        self.httpClient = [[GBHttpClient alloc] initWithBaseUrl:[NSURL URLWithString:kGBHttpClientDefaultBaseUrl] timeout:kGBHttpClientDefaultTimeout];
+        self.preference = [[GBPreference alloc] initWithFileName:kGBPreferenceDefaultFileName];
+        self.initialized = NO;
+    }
+    return self;
+}
+
+- (void) initializeWithApplicationId:(NSString *)newApplicationId credentialId:(NSString *)newCredentialId {
+
+    if (initialized) {
+        return;
+    }
+    initialized = YES;
+
+    self.applicationId = newApplicationId;
+    self.credentialId = newCredentialId;
+
+    [[GrowthbeatCore sharedInstance] initializeWithApplicationId:applicationId credentialId:credentialId];
+    if (![[GrowthbeatCore sharedInstance] client] || ![[[[[GrowthbeatCore sharedInstance] client] application] id] isEqualToString:applicationId]) {
+        [preference removeAll];
+    }
+    
+    [[GrowthAnalytics sharedInstance] initializeWithApplicationId:applicationId credentialId:credentialId];
+
+}
+
+@end
