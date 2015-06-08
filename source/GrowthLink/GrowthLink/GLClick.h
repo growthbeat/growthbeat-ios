fdf7cddf59a8123efa66ad5c26e6ac1af3f91e03
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-06-08 19:01:38 +0900

    Add GLPattern and GLClick class

---
+++ source/GrowthLink/GrowthLink/GLClick.h
@@ -0,0 +1,32 @@
+//
+//  GLClick.h
+//  Growthbeat
+//
+//  Created by Naoyuki Kataoka on 2015/06/08.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import "GBDomain.h"
+#import "GLPattern.h"
+
+@interface GLClick : GBDomain <NSCoding> {
+    
+    NSString *id;
+    GLPattern *pattern;
+    NSString *clientId;
+    BOOL open;
+    BOOL install;
+    NSDate *created;
+    NSDate *accessed;
+    
+}
+
+@property (nonatomic, strong) NSString *id;
+@property (nonatomic, strong) GLPattern *pattern;
+@property (nonatomic, strong) NSString *clientId;
+@property (nonatomic, assign) BOOL open;
+@property (nonatomic, assign) BOOL install;
+@property (nonatomic, strong) NSDate *created;
+@property (nonatomic, strong) NSDate *accessed;
+
+@end
