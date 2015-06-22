acf840fc739a4b1877c081d7f32a4091c3111ab1
y.uchiyama <yosuke.u0710@gmail.com>
2015-06-22 18:47:26 +0900

    Add GPTag

---
+++ source/GrowthPush/GrowthPush/GPTag.m
@@ -0,0 +1,99 @@
+//
+//  GPTag.m
+//  GrowthPush
+//
+//  Created by uchidas on 2015/06/22.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import "GPTag.h"
+#import "GrowthPush.h"
+#import "GBHttpClient.h"
+
+@implementation GPTag
+
+@synthesize tagId;
+@synthesize clientId;
+@synthesize value;
+
+static NSString *const kGPPreferenceTagKey = @"tags";
+
++ (GPTag *) createWithGrowthbeatClient:(NSString *)clientId credentialId:(NSString *)credentialId name:(NSString *)name value:(NSString *)value {
+    
+    NSString *path = @"/3/tags";
+    NSMutableDictionary *body = [NSMutableDictionary dictionary];
+    
+    if (clientId) {
+        [body setObject:clientId forKey:@"clientId"];
+    }
+    if (credentialId) {
+        [body setObject:credentialId forKey:@"credentialId"];
+    }
+    if (name) {
+        [body setObject:name forKey:@"name"];
+    }
+    if (value) {
+        [body setObject:value forKey:@"value"];
+    }
+    
+    GBHttpRequest *httpRequest = [GBHttpRequest instanceWithMethod:GBRequestMethodPost path:path query:nil body:body];
+    GBHttpResponse *httpResponse = [[[GrowthPush sharedInstance] httpClient] httpRequest:httpRequest];
+    if (!httpResponse.success) {
+        [[[GrowthPush sharedInstance] logger] error:@"Failed to create tag. %@", httpResponse.error];
+    }
+    
+    return [GPTag domainWithDictionary:httpResponse.body];
+    
+}
+
+- (id) initWithDictionary:(NSDictionary *)dictionary {
+    
+    self = [super init];
+    if (self) {
+        if ([dictionary objectForKey:@"tagId"] && [dictionary objectForKey:@"tagId"] != [NSNull null]) {
+            self.tagId = [[dictionary objectForKey:@"tagId"] integerValue];
+        }
+        if ([dictionary objectForKey:@"clientId"] && [dictionary objectForKey:@"clientId"] != [NSNull null]) {
+            self.clientId = [[dictionary objectForKey:@"clientId"] longLongValue];
+        }
+        if ([dictionary objectForKey:@"value"] && [dictionary objectForKey:@"value"] != [NSNull null]) {
+            self.value = [dictionary objectForKey:@"value"];
+        }
+    }
+    
+    return self;
+    
+}
+
+# pragma mark --
+# pragma mark NSCoding
+
+- (id) initWithCoder:(NSCoder *)aDecoder {
+    
+    self = [super init];
+    if (self) {
+        if ([aDecoder containsValueForKey:@"tagId"]) {
+            self.tagId = [aDecoder decodeIntegerForKey:@"tagId"];
+        }
+        if ([aDecoder containsValueForKey:@"clientId"]) {
+            self.clientId = [[aDecoder decodeObjectForKey:@"clientId"] longLongValue];
+        }
+        if ([aDecoder containsValueForKey:@"value"]) {
+            self.value = [aDecoder decodeObjectForKey:@"value"];
+        }
+    }
+    
+    return self;
+    
+}
+
+- (void) encodeWithCoder:(NSCoder *)aCoder {
+    
+    [aCoder encodeInteger:tagId forKey:@"tagId"];
+    [aCoder encodeObject:@(clientId) forKey:@"clientId"];
+    [aCoder encodeObject:value forKey:@"value"];
+    
+}
+
+
+@end
