6264319642ab92144083d6f687bf0121b75ce4a5
tamushun <shuntamu@gmail.com>
2015-07-14 15:33:15 +0900

    Create 'GMSwipeMessageRenderer.m'

---
+++ source/GrowthMessage/GrowthMessage/GMSwipeMessageRenderer.m
@@ -0,0 +1,9 @@
+//
+//  GMSwipeMessageRenderer.m
+//  Growthbeat
+//
+//  Created by 田村 俊太郎 on 2015/07/14.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import "GMSwipeMessageRenderer.h"
